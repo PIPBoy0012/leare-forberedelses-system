0e9245439ab1e4918399ad67a1fd0d9b13ef09b8
PIPBoy0012 <thiasbabs@gmail.com>
2022-11-21 10:57:53 +0100

    dgfdækmgsf0oichjvf imohftv

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,16 @@
-import 'src/Navbar.css';
-
 function Navbar(){
     return(
-        <ul>
-            <li><a class="active" href="#home">Home</a></li>
-            <li><a href="#news">News</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li style="float:right"><a href="#about">About</a></li>
-        </ul>
+        
+        <div>
+            <ul>
+                <li><a class="active" href="#home">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li style="float:right"><a href="#about">About</a></li>
+            </ul>
+        </div>
+
     );
 }
+
+export default Navbar;
