919be0b1a017737f3a168d518c7b2d3515b2aca9
PIPBoy0012 <thiasbabs@gmail.com>
2022-11-21 12:35:44 +0100

    lkhnglkfb

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar(){
         
         <div>
             <ul>
-                <li><a href="#home">Home</a></li>
+                <li><a href="MainPage.js">Home</a></li>
                 <li><a href="#news">News</a></li>
                 <li><a href="#contact">Contact</a></li>
                 <li ><a href="#about">About</a></li>
