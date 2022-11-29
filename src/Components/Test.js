e09e212365bd3d0d6877476e5189deee4bcdff6e
PIPBoy0012 <thiasbabs@gmail.com>
2022-11-29 12:39:17 +0100

    As good as it's going to get atm

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -5,7 +5,7 @@ function Object(){
         <Draggable >
             <div className='z-9 absolute cursor-move bg-white text-center rounded-lg font-bold '>
                 <div className=" z-10 p-2 bg-sky-500/100 text-black"><input className='w-32 tex bg-sky-500/100' placeholder='title' maxLength="15"></input></div>
-                <input className='inline-block align-middle w-32 h-12 mt-2 mb-2' placeholder='Beskrivelse'></input>
+                <textarea wrap='hard' className='inline-block align-middle w-32 mt-2 mb-2' placeholder='Beskrivelse'></textarea>
             </div>
 
         </Draggable>
